8016bc48308ad735ff8231ae7ce071c8d4fe8854
= <gaurav.m@orocorp.in>
2024-04-15 14:39:44 +0530

    Fixed: Changed the min height

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -6,15 +6,13 @@ export default function Home() {
     <main className="flex-center paddings mx-auto w-full max-w-screen-2xl flex-col">
       <section className="nav-padding w-full">
         <div className="flex-center relative min-h-[274px] w-ful flex-col rounded-xl text-center">
-          <div className="h-[150px] w-full">
-            <Image
-              priority
-              loading="eager"
-              layout="fill"
-              src="/jsm_resources_banner.svg"
-              alt="JavaScript Mastery Resource"
-            />
-          </div>
+          <Image
+            priority
+            loading="eager"
+            layout="fill"
+            src="/jsm_resources_banner.svg"
+            alt="JavaScript Mastery Resource"
+          />
           <h1 className="z-20 sm:heading1 heading2 mb-6 text-center text-white">
             JavaScript Mastery Resource{" "}
           </h1>
